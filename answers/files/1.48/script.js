0002b6ffbe44a4bc09c373158d5880e6954fbda4
Evgeniy-Oleynik <evgeniy.o@2muchcoffee.com>
2021-11-30 17:13:40 +0200

    1.48 task completed

--- answers/files/1.48/script.js
+++ answers/files/1.48/script.js
@@ -1,19 +1,66 @@
+let leftOperand;
+let rightOperand;
+let operator = 'ADDITION';
+let answer;
+let expression;
+
+
 function onLeftOperandChange(value) {
-  console.log('onLeftOperandChange', value)
+  console.log('onLeftOperandChange', value);
+  leftOperand = +value;
 }
 
 function onRightOperandChange(value) {
-  console.log('onRightOperandChange', value)
+  console.log('onRightOperandChange', value);
+  rightOperand = +value;
 }
 
 function onOperatorChange(value) {
-  console.log('onOperatorChange', value)
+  console.log('onOperatorChange', value);
+  operator = value;
 }
 
 function onSubmit () {
-  console.log('onSubmit')
+  console.log('onSubmit');
+  switch (operator) {
+    case 'ADDITION':
+      answer = leftOperand + rightOperand;
+      expression = `(${expression ?? leftOperand} + ${rightOperand})`;
+      break;
+    case 'SUBTRACTION':
+      answer = leftOperand - rightOperand;
+      expression = `(${expression ?? leftOperand} - ${rightOperand})`;
+      break;
+    case 'MULTIPLICATION':
+      answer = leftOperand * rightOperand;
+      expression = `(${expression ?? leftOperand} * ${rightOperand})`;
+      break;
+    case 'EXPONENTIATION':
+      answer = leftOperand ** rightOperand;
+      expression = `(${expression ?? leftOperand} xª ${rightOperand})`;
+      break;
+    case 'DIVISION':
+      answer = leftOperand / rightOperand;
+      expression = `(${expression ?? leftOperand} / ${rightOperand})`;
+      break;
+    case 'MODULUS':
+      answer = leftOperand % rightOperand;
+      expression = `(${expression ?? leftOperand} % ${rightOperand})`;
+      break;
+    case 'ROOT':
+      answer = leftOperand ** (1 / rightOperand);
+      expression = `(${expression ?? leftOperand} √ ${rightOperand})`;
+      break;
+  }
+  setResult(answer);
+  setExpression(`${expression} = ${answer}`);
 }
 
 function onClear () {
-  console.log('onClear')
+  console.log('onClear');
+  leftOperand = null;
+  rightOperand = null;
+  operator = 'ADDITION';
+  answer = null;
+  expression = null;
 }
